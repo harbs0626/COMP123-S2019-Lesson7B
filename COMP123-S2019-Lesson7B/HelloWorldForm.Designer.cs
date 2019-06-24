3e693f80ca91767eb21864c2e266e68452656e9a
harbs0626 <harbinramo@yahoo.com>
2019-06-24 14:26:47 -0400

    Added Click Me and Random Number Generator

---
+++ COMP123-S2019-Lesson7B/HelloWorldForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace COMP123_S2019_Lesson7B
+{
+    partial class HelloWorldForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.HelloWorldLabel = new System.Windows.Forms.Label();
+            this.ClickMeButton = new System.Windows.Forms.Button();
+            this.RandomNumberLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // HelloWorldLabel
+            // 
+            this.HelloWorldLabel.BackColor = System.Drawing.Color.White;
+            this.HelloWorldLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HelloWorldLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelloWorldLabel.Location = new System.Drawing.Point(235, 225);
+            this.HelloWorldLabel.Name = "HelloWorldLabel";
+            this.HelloWorldLabel.Size = new System.Drawing.Size(213, 44);
+            this.HelloWorldLabel.TabIndex = 0;
+            this.HelloWorldLabel.Text = "Hello, World!";
+            this.HelloWorldLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClickMeButton
+            // 
+            this.ClickMeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClickMeButton.Location = new System.Drawing.Point(86, 225);
+            this.ClickMeButton.Name = "ClickMeButton";
+            this.ClickMeButton.Size = new System.Drawing.Size(143, 44);
+            this.ClickMeButton.TabIndex = 1;
+            this.ClickMeButton.Text = "Click Me!";
+            this.ClickMeButton.UseVisualStyleBackColor = true;
+            this.ClickMeButton.Click += new System.EventHandler(this.ClickMeButton_Click);
+            // 
+            // RandomNumberLabel
+            // 
+            this.RandomNumberLabel.AutoSize = true;
+            this.RandomNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RandomNumberLabel.Location = new System.Drawing.Point(229, 182);
+            this.RandomNumberLabel.Name = "RandomNumberLabel";
+            this.RandomNumberLabel.Size = new System.Drawing.Size(44, 31);
+            this.RandomNumberLabel.TabIndex = 3;
+            this.RandomNumberLabel.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(229, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "00";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // HelloWorldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RandomNumberLabel);
+            this.Controls.Add(this.ClickMeButton);
+            this.Controls.Add(this.HelloWorldLabel);
+            this.Name = "HelloWorldForm";
+            this.Text = "Hellow World Form";
+            this.Load += new System.EventHandler(this.HelloWorldForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HelloWorldLabel;
+        private System.Windows.Forms.Button ClickMeButton;
+        private System.Windows.Forms.Label RandomNumberLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ COMP123-S2019-Lesson7B/HelloWorldForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lesson7B
+{
+    public partial class HelloWorldForm : Form
+    {
+        /// <summary>
+        /// This is the HelloWorldForm Constructor
+        /// </summary>
+        public HelloWorldForm()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        /// <summary>
+        /// The start method is used for property initialization
+        /// </summary>
+        public void Start()
+        {
+            this.HelloWorldLabel.BackColor = Color.Black;
+            this.HelloWorldLabel.ForeColor = Color.White;
+            this.HelloWorldLabel.Text = "Hello, Harbin!";
+
+        }
+
+        /// <summary>
+        /// This method is the event handler for the ClickMeButton Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClickMeButton_Click(object sender, EventArgs e)
+        {
+            HelloWorldLabel.Text = (HelloWorldLabel.Text == "Clicked!") ? "Unclicked!" : "Clicked!";
+
+            GenerateRandomNunber();
+        }
+
+        /// <summary>
+        /// This method generates a random integer from 1 to 49;
+        /// </summary>
+        private void GenerateRandomNunber()
+        {
+            Random _rand = new Random();
+            int randomNo = _rand.Next(0, 49);
+            //RandomNumberLabel.Text = (randomNo.ToString().Length == 1) ? "0" + randomNo.ToString() : randomNo.ToString();
+            RandomNumberLabel.Text = (new Random().Next(1, 49) + 1).ToString();
+        }
+
+        private void HelloWorldForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
